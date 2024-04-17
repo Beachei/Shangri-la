724cd81d03b631719985870bef1a0b1a2b270920
beachei <isaiaprecieux2@gmail.com>
2024-04-17 17:54:55 +0200

    Logement js available and unavailable

--- Js/Restaurant.js
+++ Js/Restaurant.js
@@ -1,29 +1,21 @@
-/* Restaurant */
 
-document.addEventListener("DOMContentLoaded", ()=>{
-
+document.addEventListener("DOMContentLoaded", function(){
+    /* Restaurant */
     const resto = document.getElementById("Resto&bar");
     const cuisine = document.getElementById("Cuisine");
     const restobtn = document.getElementById("restoBtn");
     const cuisinebtn = document.getElementById("cuisineBtn");
     const btntocuisine = document.getElementById("bottomBtn");
 
-    restobtn.addEventListener("click", (e) => {
+
+    restobtn.addEventListener("click", ()=>{
        resto.classList.add('d-block');
        resto.classList.remove('d-none');
        resto.classList.add('show');
        cuisine.classList.add('d-none');
        cuisine.classList.remove('d-block');
     })
-    btntocuisine.addEventListener("click", (e) => {
-        cuisine.classList.remove('d-none');
-        cuisine.classList.add('d-block');
-        cuisine.classList.add('show');
-        resto.classList.remove('d-block');
-        resto.classList.add('d-none');
-     })
-
-    cuisinebtn.addEventListener("click", (e) => {
+    btntocuisine.addEventListener("click", ()=>{
         cuisine.classList.remove('d-none');
         cuisine.classList.add('d-block');
         cuisine.classList.add('show');
@@ -31,15 +23,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
         resto.classList.add('d-none');
     })
 
-    var elementsWithBackgroundImages = document.querySelectorAll('[style*="background-image"]');
-    
-    elementsWithBackgroundImages.forEach(function(element) {
-        var backgroundImage = element.style.backgroundImage;
-        var imageUrl = backgroundImage.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+    cuisinebtn.addEventListener("click", ()=> {
+        cuisine.classList.remove('d-none');
+        cuisine.classList.add('d-block');
+        cuisine.classList.add('show');
+        resto.classList.remove('d-block');
+        resto.classList.add('d-none');
+    })
 
-        var img = new Image();
-        img.src = imageUrl;
-    });
 
 })
    
