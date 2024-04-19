260e7e4c6ff6813c6181b3660b2950a19f52ea77
beachei <isaiaprecieux2@gmail.com>
2024-04-19 18:03:22 +0200

    animation

---
+++ Js/Voyage.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const a = document.getElementById('a');
+    const b = document.getElementById('b');
+    const c = document.getElementById('c');
+    const d = document.getElementById('d');
+    const e = document.getElementById('e');
+    const f = document.getElementById('f');
+    const g = document.getElementById('g');
+    const h = document.getElementById('h');
+    const i = document.getElementById('i');
+    const j = document.getElementById('j');
+    const animScroll = document.getElementById('animScroll');
+    function scaleAnim(){
+        a.classList.add("scaleRN7");
+    }
+    function scaleRemove(){
+        a.classList.remove("scaleRN7");
+    }
+    setTimeout(scaleAnim,3000)
+    animScroll.addEventListener('c')
+})
